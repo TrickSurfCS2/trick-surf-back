72788ead628962d90d84c2eeb95972101eeb1439
ivan <ivan.kornilov@nordclan.com>
2023-09-22 14:51:06 +0400

    fix: types

--- src/api/rest/middleware/prometheus.middleware.ts
+++ src/api/rest/middleware/prometheus.middleware.ts
@@ -7,26 +7,30 @@ const prometheusMiddleware = (request: Request, response: Response, next: NextFu
   const userAgent = headers['user-agent'];
   const acceptLanguage = headers['accept-language'];
 
-  // Извлечение типа браузера
-  const browserRegex = /(?:Chrome|Firefox|Safari|Opera|IE|Edge|Yandex|UCBrowser)/i;
-  const browserMatch = userAgent.match(browserRegex);
-  const browser = browserMatch ? browserMatch[0] : 'Unknown';
+  if (userAgent) {
+    // Извлечение типа браузера
+    const browserRegex = /(?:Chrome|Firefox|Safari|Opera|IE|Edge|Yandex|UCBrowser)/i;
+    const browserMatch = userAgent.match(browserRegex);
+    const browser = browserMatch ? browserMatch[0] : 'Unknown';
 
-  // Извлечение локации
-  const locationRegex = /(?:ru|en|fr|de|es)/i;
-  const locationMatch = acceptLanguage.match(locationRegex);
-  const location = locationMatch ? locationMatch[0] : 'Unknown';
+    if (browser) {
+      PromCounterBrowser.inc({ browser });
+    }
+  }
+
+  if (acceptLanguage) {
+    // Извлечение локации
+    const locationRegex = /(?:ru|en|fr|de|es)/i;
+    const locationMatch = acceptLanguage.match(locationRegex);
+    const location = locationMatch ? locationMatch[0] : 'Unknown';
+
+    if (location) {
+      PromCounterLocation.inc({ location });
+    }
+  }
 
   if (route) {
     PromCounterRoute.inc({ route });
-  }
-
-  if (location) {
-    PromCounterLocation.inc({ location });
-  }
-
-  if (browser) {
-    PromCounterBrowser.inc({ browser });
   }
 
   next();
